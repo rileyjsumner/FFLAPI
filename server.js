bfc9ec54bcc8cf7b53c8f8e5a25c8d0cda582581
Riley Sumner <rsumner@wisc.edu>
2020-07-24 11:57:34 -0500

    sleep

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ let cors = require('cors');
 const PORT = 1376;
 const BASE_URL = "https://api.myfantasyleague.com/2020";
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
 async function getLeagues() {
     try {
         return axios.get(`${BASE_URL}/export?TYPE=leagueSearch&SEARCH=SafeLeagues+Dynasty&JSON=1`);
@@ -74,13 +82,20 @@ app.get('/leagues', async (req, res) => {
     });
 
     console.log("League IDS: " + idList.length);
-    for(let i = 0; i < idList.length; i++) {
+    for(let i = 0; i < 30; i++) {
         await getDraftResults(idList[i]).then((resp) => {
             // console.log(resp.data.draftResults.draftUnit);
-            if(resp.data.draftResults.draftUnit.draftPick !== undefined) {
-                if(resp.data.draftResults.draftUnit.draftPick[0].timestamp > 1588309200) {
-                    draftResult = draftResult.concat(resp.data.draftResults.draftUnit.draftPick);
-                    console.log("Good ID:", idList[i]);
+            if (resp.data.draftResults.draftUnit.draftPick !== undefined) {
+                if (resp.data.draftResults.draftUnit.draftPick[0].timestamp > 1588309200) {
+                    if(resp.data.draftResults.draftUnit.draftPick.length > 50) {
+                        draftResult = draftResult.concat({
+                            id: idList[i],
+                            draft: resp.data.draftResults.draftUnit.draftPick
+                        });
+                        console.log("Good ID:", idList[i]);
+                    } else {
+                        console.log("ROOKIE DRAFT: " + resp.data.draftResults.draftUnit.draftPick.length);
+                    }
                 }
 
             } else {
@@ -89,6 +104,8 @@ app.get('/leagues', async (req, res) => {
         }).catch((err) => {
             console.error(err);
         });
+        sleep(2000);
+
     }
     res.send(draftResult);
 
